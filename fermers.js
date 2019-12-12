6e63458f68c400d0eeb4bd188080caa9458136f0
Adesayo <53871688+Adesayo@users.noreply.github.com>
2019-12-12 16:02:09 +0100

    Add files via upload

---
+++ fermers.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+  jQuery(".trigger").click(function(){
+    jQuery(".jmenu").slideToggle();
+  });
+});
+
+
+
+// jQuery(document).ready(function(){
+//   jQuery(".toggle span").click(function(){
+//     jQuery("ul").toggleClass("active")
+//   });
+// });
